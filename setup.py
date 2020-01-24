b58387cbeafe9bd97c53e929ad085b6e68dcb4ea
binares <binares@protonmail.com>
2020-01-25 00:50:58 +0200

    [fix] setup.py include data files

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+packages = find_packages(exclude=['examples','test'])
+
+if 'uxs._data' not in packages:
+    packages.append('uxs._data')
+
 setup(
    name='uxs',
    version='0.1.0',
@@ -13,7 +18,8 @@ setup(
    url='https://github.com/binares/uxs',
    author='binares',
    author_email='binares@protonmail.com',
-   packages=find_packages(exclude=['examples','test']),
+   packages=packages,
+   package_data={'uxs._data': ['*']},
    # 3.5.3 is required by aiphttp>=3.0, which in turn
    # is required by ccxt
    python_requires='>=3.5.3',
